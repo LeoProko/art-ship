4177df7754f88b08c6be5f4ddc2a86a04b6d226f
Leo Proko <prokop4uk.lyonya@yandex.ru>
2021-01-03 02:30:12 +0300

    [canvas_js] add curves.js

--- canvas_js/draft_of_canvas_js/curves.js
+++ canvas_js/draft_of_canvas_js/curves.js
@@ -1,29 +1,48 @@
 canvas = document.getElementById('curves');
-context = canvas.getContext('2d');
-//canvas.width = window.innerWidth;
-//canvas.height = window.innerHeight;
-canvas.width = 500;
-canvas.height = 500;
 
+size = 500;
+img = new ArtShip(size, size, canvas);
+context = img.context;
+img.background(255, 0, 255, 0.2);
 
+step = size / 10;
 
-context.moveTo(100, 100);
-context.lineTo(600, 500);
-context.stroke();
+function brush_hair (coordinates) {
+    for (let brush_stroke = 0; brush_stroke < size / 20; brush_stroke++) {
+        let points = [];
+        for (let coordinate of coordinates) {
+            x = img.random(coordinate[0] - size / 60, coordinate[0] + size / 60);
+            y = img.random(coordinate[1] - size / 60, coordinate[1] + size / 60);
+            points.push([x, y]);
+        }
 
-context.moveTo(100, 100);
-context.bezierCurveTo(
-    150, 150,
-    150, 400,
-    300, 300
-);
-context.stroke();
+        for (let point of points) {
+            img.circle(point[0], point[1], 5);
+            img.fill(255, 255, 0, 0.5);
+        }
 
+        img.curve(points);
+        img.stroke(0, 0, 0, 3, 0.5);
+    }
+}
 
-context.beginPath();
-context.ellipse(150, 150, 100, 50, 1, 0, 7);
-context.stroke();
+coordinates = [
+    [0, 0],
+    [0.4 * size, 0.2 * size],
+    [0.2 * size, 0.9 * size],
+    [0.8 * size, 0.8 * size],
+    [size, 0.3 * size]
+];
 
-context.beginPath();
-context.rect(100, 100, 100, 200);
-context.stroke();
+brush_hair(coordinates);
+
+pixel_image = context.getImageData(0, 0, 1000, 1000);
+for (let i = 0; i < pixel_image.data.length; i += 4 * 40) {
+    if (i % pixel_image.width === 0)
+        i += 4 * 40 * pixel_image.width;
+    pixel_image.data[i + 0] = 0;  // R value
+    pixel_image.data[i + 1] = 0;    // G value
+    pixel_image.data[i + 2] = 0;  // B value
+    pixel_image.data[i + 3] = 255;  // A value
+}
+context.putImageData(pixel_image, 0, 0);
