6a6a629499bb5f1f8df9dc9d04f37f8a7014e826
leoproko <prokop4uk.lyonya@yandex.ru>
2021-01-17 23:38:08 +0300

    [canvas_js/spliner] add curls and slicing

--- canvas_js/spliner/spliner.js
+++ canvas_js/spliner/spliner.js
@@ -5,34 +5,41 @@ let context = img.context;
 
 function draw() {
     img.background(0, 0, 0);
-    for (let y = 0; y <= img.height; y += img.ratio(img.random(4, 30))) {
+    for (let y = 0; y <= img.height * 1.3; y += img.ratio(img.random(3, 10))) {
         let line = [];
-        for (let x = img.ratio(img.random(2, 20)); x <= img.width - img.ratio(img.random(2, 20)); x += img.ratio(img.random(4, 100))) {
-            line.push([x, y + img.random(-img.ratio(10), img.ratio(10))]);
+        for (let x = 0; x <= img.width * 1.3; x += img.ratio(img.random(4, 150))) {
+            line.push([x + img.random(-img.ratio(10), img.ratio(20)), y + img.random(-img.ratio(20), img.ratio(8))]);
+            x += img.int(img.random(-img.ratio(100), img.ratio(30)));
         }
         multi_curve(line);
     }
 }
 
 function multi_curve(line) {
-    steps = img.int(img.random(3, 20));
-    while (steps--) {
-        let new_line = [];
-        for (let i = 0; i < line.length; ++i) {
-            let x = line[i][0] + img.random(-img.ratio(100), img.ratio(50));
-            let y = line[i][1] + img.random(-img.ratio(100), img.ratio(100));
-            new_line.push([x, y]);
-            if (Math.random() > 0.9 && new_line.length > 1) {
-                img.print(new_line);
-                img.curve(new_line);
-                img.stroke(1, 255, 255, 255, 0.7);
-                new_line = [];
-                ++i;
+    let curves = [];
+    let i = 0;
+    while (i < line.length) {
+        let steps = img.int(img.random(10, 40));
+        let start_i = i;
+        let break_i = start_i + img.int(img.random(1, (line.length - start_i) * 0.3));
+        while (steps--) {
+            let new_line = [];
+            for (let i = start_i; i < break_i; ++i) {
+                let x = line[i][0] + img.random(-img.ratio(100), img.ratio(50));
+                let y = line[i][1] + img.random(-img.ratio(100), img.ratio(100));
+                new_line.push([x, y]);
+                if (Math.random() > 0.99) {
+                    break;
+                }
             }
+            curves.push(new_line);
         }
-        if (new_line.length > 1) {
-            img.curve(new_line);
-            img.stroke(5, 255, 255, 255, 0.7);
+        i = break_i + img.int(img.random(1, 5));
+    }
+    for (let curve of curves) {
+        if (curve.length > 1) {
+            img.curve(curve);
+            img.stroke(3, 255, 255, 255, 0.35);
         }
     }
 }
